c2ceaa7dd6d88827a998bb40ac7677342a902fea
unknown <kgrigoryan@dsnci.com>
2022-01-03 22:15:07 +0300

    next

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,13 @@ function nextClick(){
     const firstPage = document.getElementById("first-page");
     firstPage.classList.toggle("form-shoved");
     secondPage.classList.toggle("form-shoved");
+    localStorage.setItem('stage', 1);
+}
+
+function toQRClick(){
+    const secondPage = document.getElementById("second-page");
+    const threePage = document.getElementById("three-page");
+    threePage.classList.toggle("form-shoved");
+    secondPage.classList.toggle("form-shoved");
+    localStorage.setItem('stage', 2);
 }
